4a29c6158d69c8156efe1f1fd9e8b506d29a4680
m7abdelghany <mohamedabdelghany113@gmail.com>
2023-03-01 01:01:14 +0200

    register and login

--- Program.cs
+++ Program.cs
@@ -16,7 +16,8 @@ namespace Airbnbfinal
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
